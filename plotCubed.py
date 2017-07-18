f98e33e24b91bfd533d6845330d4d9c40636991f
Devin Suttles <devinfnsuttles@gmail.com>
2017-07-18 14:49:00 -0500

    Changed plotCubed.py

--- plotCubed.py
+++ plotCubed.py
@@ -37,5 +37,19 @@ def yAvg(d):
 fig1 = plt.figure()
 
 dataNum = 2
-d = 10*np.random.rand(dataNum, 1)#10=x&y limit and # of points. 3=# of points * 3
+d = 10*np.random.rand(dataNum, 1)#10=x&y limit and # of points. 3=# of points * 3     
+
+xAvg = xAvg(d) #x avg
+xsAvg = xsAvg(d) #x^2 avg
+xyAvg = xyAvg(d) #xy avg
+yAvg = yAvg(d) #y avg
+xxAvg=xAvg*xsAvg
+#y=Ax^2+Bx+C
+B=(xAvg*yAvg*math.pow(xAvg,4)-xsAvg*yAvg*math.pow(xAvg,3))/(math.pow(xAvg,6)-math.pow(xxAvg,2))
+C=(xsAvg*yAvg*xsAvg-xAvg*yAvg*xxAvg)/(math.pow(xAvg,6)-math.pow(xxAvg,2))
+A=yAvg-B*xAvg-C*xsAvg
+
+z = np.arange(-10,100,1)
+ax = plt.plot(d,'go') #'Go' displays the datapoints
+ax = plt.plot(math.pow(z,2),A+z*A+B)
 
