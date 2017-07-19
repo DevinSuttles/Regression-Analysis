f4e31e3e7b36a771320ed88bfde1ffd5864c0a9f
Ian Yake <iangyake@gmail.com>
2017-07-18 22:43:31 -0500

    stuff

--- plotCubed.py
+++ plotCubed.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def xyAvg(d):#assumes that x increments as an integer
+def xyAvgf(d):#assumes that x increments as an integer
     sum = 0.0
     counter = 0
     for i in d:
@@ -10,13 +10,13 @@ def xyAvg(d):#assumes that x increments as an integer
         counter += 1
     return sum/d.size
 
-def xAvg(d):#assumes that x increments as an integer
+def xAvgf(d):#assumes that x increments as an integer
     sum = 0.0
     for i in d:
         sum += 1
     return sum/d.size
 
-def xsAvg(d):#x squared avg, assumes that x increments as an integer
+def xsAvgf(d):#x squared avg, assumes that x increments as an integer
     sum = 0.0
     counter  = 0
     for i in d:
@@ -24,32 +24,64 @@ def xsAvg(d):#x squared avg, assumes that x increments as an integer
         counter += 1
     return sum/d.size
 
-def yAvg(d):
+def yAvgf(d):
     sum = 0
     for i in d:
         sum += i
     return sum/d.size
 
+def xxxxAvgf(d):
+    sum = 0
+    counter = 0
+    for i in d:
+        sum += math.pow(counter,4)
+        counter += 1
+    return sum/d.size
+
+def xxyAvgf(d):
+    sum = 0
+    counter = 0
+    for i in d:
+        sum += i*math.pow(counter,2)
+        counter += 1
+    return sum/d.size
+
+def xxxAvgf(d):
+    sum = 0
+    counter = 0
+    for i in d:
+        sum += math.pow(counter,3)
+        counter += 1
+    return sum/d.size
+
 fig1 = plt.figure()
 
-dataNum = 6
+dataNum = 30
 d = 10*np.random.rand(dataNum, 1)#10=x&y limit and # of points. 3=# of points * 3     
 
-xAvg = xAvg(d) #x avg
-xsAvg = xsAvg(d) #x^2 avg
-xyAvg = xyAvg(d) #xy avg
-yAvg = yAvg(d) #y avg
-xxxAvg=xAvg*xsAvg #x^3 avg
+xAvg = xAvgf(d) #x avg
+xxAvg = xsAvgf(d) #x^2 avg
+xyAvg = xyAvgf(d) #xy avg
+yAvg = yAvgf(d) #y avg
+
+#xxxAvg=xAvg*xsAvg #x^3 avg
+xxxAvg=xxxAvgf(d)
+
+xxxxAvg=xxxxAvgf(d)
+xxyAvg=xxyAvgf(d)
 #y=Ax^2+Bx+C
-A=((xsAvg*yAvg*xsAvg)-(xAvg*yAvg*xxxAvg))/((xsAvg*xsAvg*xsAvg)-(math.pow(xxxAvg,2)))
-B=((xAvg*yAvg*math.pow(xsAvg,2))-(xsAvg*yAvg*xsAvg*xAvg))/((xsAvg*xsAvg*xsAvg)-(math.pow(xxxAvg,2)))
-C=yAvg-B*xAvg-A*xsAvg
+#A=((xsAvg*yAvg*xsAvg)-(xAvg*yAvg*xxxAvg))/((xsAvg*xsAvg*xsAvg)-(math.pow(xxxAvg,2)))
+#B=((xAvg*yAvg*math.pow(xsAvg,2))-(xsAvg*yAvg*xsAvg*xAvg))/((xsAvg*xsAvg*xsAvg)-(math.pow(xxxAvg,2)))
+#C=yAvg-B*xAvg-A*xsAvg
+A=((xyAvg-xAvg*yAvg)*(xxxxAvg-xxAvg*xxAvg)-(xxyAvg-xxAvg*yAvg)*(xxxAvg-xAvg*xxAvg))/((xxAvg-xAvg*xAvg)*(xxxxAvg-xxAvg*xxAvg)-(xxxAvg-xAvg*xxAvg)*(xxxAvg-xAvg*xxAvg))
+B=((xxyAvg-xxAvg*yAvg)*(xxAvg-xAvg*xAvg)-(xyAvg-xAvg*yAvg)*(xxxAvg-xAvg*xxAvg))/((xxAvg-xAvg*xAvg)*(xxxxAvg-xxAvg*xxAvg)-math.pow(xxxAvg-xAvg*xxAvg,2))
+C=yAvg-B*xAvg-A*xxAvg
 
 
 
-z = np.arange(-10,10,1)
-plt.ylim(-20,40)
-plt.xlim(-20,40)
+z = np.arange(-100,100,1)
+plt.ylim(-100,100)
+plt.xlim(-100,100)
 ax = plt.plot(d,'go') #'Go' displays the datapointsax = plt.plot(z,C*z*z+B*z+A)
 ax = plt.plot(z,A*z*z+B*z+C)
 
